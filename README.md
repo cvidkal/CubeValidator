1bc048f9a29d0bb82f715afea190baa7c7e272a8
chenguojun <chenguojun@yogorobot.com>
2023-06-11 00:11:24 +0800

    single board version

---
+++ README.md
@@ -0,0 +1,16 @@
+# YOGO CPP 模版
+
+```
+.
+├── .gitignore
+├── CMakeLists.txt
+├── README.md         # 项目说明
+├── conanfile.py      # conan依赖管理
+├── include
+│   └── template.hpp  # 对外暴露的头文件
+├── src
+│   └── template.cpp 
+├── test
+│   └── test.cpp      # 单元测试文件
+└── version.txt
+```
---
+++ conanfile.py
@@ -0,0 +1,57 @@
+from conans import ConanFile
+from conans.tools import load
+import re
+import os
+
+
+class Template(ConanFile):
+    python_requires = "cmake-build-helper/[>=0.1]@navi/develop", "qmake-build-helper/[>=0.1]@navi/develop"
+    name = "template"
+    license = "None"
+    author = "YOGO Robot"
+    url = "www.yogorobot.com"
+    description = "template"
+    topics = ("basic")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+    }
+    default_options = {}
+    generators = "qmake", "cmake", "vscode"
+    scm = {
+        "type": "git",
+        "url": "auto",
+        "revision": "auto",
+        "password": os.environ.get("SECRET", None)
+    }
+
+    requires = (
+
+    )
+
+    def set_version(self):
+        content = load(os.path.join(self.recipe_folder, "version.txt"))
+        self.version = content.strip()
+
+    def build_with_cmake(self):
+        cmake = self.python_requires["cmake-build-helper"].module.CMakeBuildEnvironment(
+            self)
+
+        args = []
+        cmake.configure(self.source_folder, args)
+        cmake.build()
+
+    def build(self):
+        self.build_with_cmake()
+
+    def package(self):
+        self.copy("*.h*", src="include", dst="include", keep_path=True)
+        self.copy("*.so*", dst="lib", keep_path=False)
+
+        # for executable
+
+        # deps = self.deps_cpp_info.deps
+        # for dep in deps:
+        #     dep_cpp_info = self.deps_cpp_info[dep]
+        #     for lib in dep_cpp_info.lib_paths:
+        #         self.copy("*.so*", dst="lib", src=lib, keep_path=False)
+        #         self.copy("*.a", dst="lib", src=lib, keep_path=False)
